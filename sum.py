8c611c01e280119cf343bfebdc7ffc3c0e70bd8a
Shivani Sharma <shivani.sharma7@globallogic.com>
2026-05-21 19:10:21 +0530

    modified sum file

--- sum.py
+++ sum.py
@@ -2,7 +2,7 @@
 
 num1 = 10
 num2 = 20
+num3 = 30
+sum = num1 + num2 + num3
 
-sum = num1 + num2
-
-print(f"The sum of {num1} and {num2} is {sum}")
+print(f"The sum of {num1}, {num2}, and {num3} is {sum}")
